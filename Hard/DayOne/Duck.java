7240a9e27b45b7bd0dc2de9ad6ed50b0a9041135
JericCabay10 <jericcabay10@gmail.com>
2025-10-21 09:42:07 +0000

    Update DuckNumbers

--- Hard/DayOne/Duck.java
+++ Hard/DayOne/Duck.java
@@ -1,6 +1,9 @@
 package Hard.DayOne;
 
+import java.util.*;
+
 public class Duck {
+    static Scanner input = new Scanner(System.in);
     public static void DuckNumber() {
 		while(true) {
 			System.out.println("The Duck Number has 0 in the digit example 123045, 123450");
@@ -8,19 +11,43 @@ public class Duck {
 			
 			System.out.print("Enter a digit: ");
 			int num = input.nextInt();
+
+            if(num == 0) {
+                System.out.println("Exit Function...");
+                break;
+            }
 			
-			boolean isDuck = true;
+			boolean hasZero = false;
 			
 			int digit = 0;
 			while(num != 0) {
 				digit = num % 10;
-				if(digit != 0) {
-					isDuck = false;
-					break;
+				if(digit == 0) {
+					hasZero = true;
+                    break;
 				}
 				num /= 10;
 			}
+
+            if (hasZero) {
+                System.out.println(num + " is a Duck number.");
+            } else {
+                System.out.println(num + " is NOT a Duck number.");
+            }
 		}
 	}
-    
+ 
+    public static void main(String[] args) {
+        while(true) {
+            System.out.print("Choose a function: ");
+            int choice = input.nextInt();
+
+            if(choice == 0) {
+                System.out.println("Exit System...");
+                break;
+            }else if(choice == 1) {
+                DuckNumber();
+            }
+        }
+    }
 }
